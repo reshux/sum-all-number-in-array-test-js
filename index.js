75866722d67d07f411c7761bdc590aac682658f9
reshux <duruhanunsal@gmail.com>
2019-04-02 20:50:44 +0000

    Recursion implemented

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 function sumItems(array) {
   // Sum all the numbers in the array
+  return array.reduce((a, b) => {
+    if (Array.isArray(b)) {
+      return a + sumItems(b);
+    }
+    return a + b;
+  }, 0);
 }
 
 module.exports = sumItems;
